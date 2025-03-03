e92c45d287bd0abd3fb7bbd71b3c538d4aee438f
Francis Hamzagic <francishamzagic@gmail.com>
2025-03-03 09:54:58 -0300

    Update Connect.js

--- src/db/Connect.js
+++ src/db/Connect.js
@@ -4,19 +4,13 @@ require('dotenv').config();
 const url = process.env.DB_URL;
 const client = new MongoClient(url);
 
-const dbName = 'fran-db';
+// test purposes only, but I believe this db does not exist anymore
+const dbName = process.env.DB_NAME;
 
 const DbConnection = async () => {
     await client.connect();
     console.log('connected to server');
     return 'done';
 }
-
-// const connect = () => {
-//     DbConnection()
-//     .then(res => console.log(res))
-//     .catch(err => console.log(err))
-// //.finally(() => client.close());
-// }
 
 module.exports = { DbConnection };
